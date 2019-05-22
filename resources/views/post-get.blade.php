cc0249b408acd9278f37d7e5b8556acbc6a04ae9
Aleksandr <kuang87@mail.ru>
2019-05-22 23:04:35 +0300

    Add LikePost button by different users

--- resources/views/post-get.blade.php
+++ resources/views/post-get.blade.php
@@ -4,18 +4,24 @@
 
 @section('content')
 
-    <ul>
-        <li>{{ $post->title }}</li>
-        <li>{{ $post->user->name }}</li>
-        <li>{{ $post->content }}</li>
-    </ul>
-    <p>Category: {{ $post->category->name }}</p>
-    <p>Tags:
-        @foreach($post->tags as $tag)
-            #{{ $tag->name }}
-        @endforeach
+    @isset($post)
+        <ul>
+            <li>{{ $post->title }}</li>
+            <li>{{ $post->user->name }}</li>
+            <li>{{ $post->content }}</li>
+        </ul>
+        <p>Category: {{ $post->category->name }}</p>
+        <p>Tags:
+            @foreach($post->tags as $tag)
+                #{{ $tag->name }}
+            @endforeach
+            </p>
+        <p class="like">
+            <a href="{{BASE_URL. 'posts/' . $post->id . '/like'}}">
+                Мне нравится <img src="{{BASE_URL. 'static/img/likebtn.png'}}"></a>
+            {{\Aleksandr\Model\Like::where('idpost', $post->id)->where('isLike', 1)->count()}}
         </p>
-    <p class="like"><a href="{{BASE_URL. 'posts/' . $post->id . '/like'}}">Мне нравится</a></p>
 
-    <p><a href="{{BASE_URL . 'posts/edit/' . $post->id}}">Редактировать</a></p>
+        <p><a href="{{BASE_URL . 'posts/edit/' . $post->id}}">Редактировать</a></p>
+    @endisset
 @endsection
--- src/Action/PostLikeAction.php
+++ src/Action/PostLikeAction.php
@@ -5,25 +5,31 @@ namespace Aleksandr\Action;
 
 use Aleksandr\Model\Like;
 use Aleksandr\Model\Post;
+use Aleksandr\Service\UserAuthentication;
 use GuzzleHttp\Psr7\ServerRequest;
 
 class PostLikeAction
 {
     public function __invoke(ServerRequest $request)
     {
-        $post = Post::find($request->getAttribute('id'));
+        if (UserAuthentication::check()) {
+            $post = Post::find($request->getAttribute('id'));
 
-        $like = new Like();
-        $like->idpost = $post->id;
-        $like->iduser = 1;
-        $like->isLike = 1;
+            if (!empty($post)) {
+                $like = new Like();
+                $like->idpost = $post->id;
+                $like->iduser = $_SESSION['user']->id;
+                $like->isLike = 1;
 
-        try{
-            $like->save();
-        } catch (\Exception $exception){
-            header('Location: ' . BASE_URL . 'posts/' . $post->id);
+                try {
+                    $like->save();
+                    throw new \Exception('ERRRORR');
+                } catch (\Exception $exception) {
+                    //echo $exception->getMessage();
+                    header('Location: ' . BASE_URL . 'posts/' . $post->id);
+                }
+            }
         }
-
-        return view('post-get', ['post' => $post]);
+        return view('post-get', ['post' => $post ?? null]);
     }
 }
